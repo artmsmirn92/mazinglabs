1cac6f91a15265e32918e803c93fa3b7c56e8603
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2022-02-23 10:12:37 +0300

    new policy for google + third-party

---
+++ scripts.js
@@ -0,0 +1,46 @@
+function open_url(url)
+{
+    window.open(url);
+}
+
+function opendoc_trirdparty()
+{
+    open_url("third-party-services.html");
+}
+
+function opendoc_policy()
+{
+    open_url("policy.html");
+}
+
+function write_company_name()
+{
+    document.write("Mazing Labs");
+}
+
+function write_current_year()
+{
+    document.write(new Date().getFullYear());
+}
+
+function get_link_thirdparty(title)
+{
+    document.write("<a href=\"javascript:opendoc_trirdparty();\">" + title + "</a>")
+}
+
+function get_link_policy(title)
+{
+    document.write("<a href=\"javascript:opendoc_policy();\">" + title + "</a>")
+}
+
+function get_link_support_mail()
+{
+    let mail = "mgc0mob@gmail.com";
+    document.write("<a href =\"mailto: " + mail + "\">"+ mail + "</a>")
+}
+
+
+function write_link(url, title)
+{
+    document.write("<a href=\"javascript:opendoc_policy();\">" + title + "</a>")
+}
